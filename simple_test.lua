8ef52e236771a88b165b4ebb2048892f4d1da7ae
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-06-19 11:10:16 -0300

    fix problem with spaces

--- simple_test.lua
+++ simple_test.lua
@@ -4,37 +4,37 @@
 -- License: MIT
 
 local assertions = {
-  equal = function(a, b)
-    assert(a == b)
-  end,
+	equal = function(a, b)
+		assert(a == b)
+	end,
 
 	not_equal = function(a, b)
 		assert(a != b)
 	end,
 
-  throw = function(message, method, params)
-    local raised_error = false
+	throw = function(message, method, params)
+		local raised_error = false
 
-    xpcall(function()
-      method(table.unpack(params))
-    end, function(err)
-      raised_error = true
-      assert(string.find(err, message))
-    end)
+		xpcall(function()
+			method(table.unpack(params))
+		end, function(err)
+		raised_error = true
+		assert(string.find(err, message))
+	end)
 
-    if not raised_error then error('should throw error') end
-  end
+	if not raised_error then error('should throw error') end
+end
 }
 
 local format = string.format
 
 local function test(name, func)
-  xpcall(function()
-    func(assertions)
-    print(format('[pass] %s', name))
-  end, function(err)
-    print(format('[fail] %s : %s', name, err))
-  end)
+	xpcall(function()
+		func(assertions)
+		print(format('[pass] %s', name))
+	end, function(err)
+	print(format('[fail] %s : %s', name, err))
+end)
 end
 
 return test
