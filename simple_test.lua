581b146ca28f250e4c6e3a54f969b23094d80293
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-06-20 23:22:29 -0300

    add assert.delta method to do floating point comparison

--- simple_test.lua
+++ simple_test.lua
@@ -9,7 +9,7 @@ local assertions = {
   end,
 
   not_equal = function(a, b)
-    assert(a != b)
+    assert(a ~= b)
   end,
 
   throw = function(message, method, params)
@@ -23,14 +23,30 @@ local assertions = {
     end)
 
     if not raised_error then error('should throw error') end
+  end,
+
+  delta = function(a, b, delta)
+    local diff = math.abs(a - b)
+    delta = delta or 0.00001
+
+    if a == b then
+      return true
+    elseif a == 0 or b == 0 then
+      assert(diff < delta)
+    else
+      local abs_a = math.abs(a)
+      local abs_b = math.abs(b)
+
+      assert((diff / (abs_a + abs_b)) < delta)
+    end
   end
 }
 
 local format = string.format
 
-local function test(name, func)
+local function test(name, callback)
   xpcall(function()
-    func(assertions)
+    callback(assertions)
     print(format('[pass] %s', name))
   end, function(err)
     print(format('[fail] %s : %s', name, err))
