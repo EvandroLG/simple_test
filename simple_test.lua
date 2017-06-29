e845c1e83764449da96d8eebf86b073856b593f0
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2017-06-28 21:54:08 -0300

    add fault message to each assert method

--- simple_test.lua
+++ simple_test.lua
@@ -3,61 +3,74 @@
 -- https://github.com/evandrolg
 -- License: MIT
 
+local format = string.format
+
+local colors = {
+  green = { '\27[32m', '\27[0m' },
+  red = { '\27[31m', '\27[0m' }
+}
+
 local assertions = {
-  equal = function(a, b)
-    assert(a == b)
+  equal = function(a, b, msg)
+    local message = msg or format('%s%s%s expected but was %s%s%s',
+          colors.green[1], a, colors.green[2], colors.red[1], b, colors.red[2])
+
+    assert(a == b, message)
   end,
 
-  not_equal = function(a, b)
-    assert(a ~= b)
+  not_equal = function(a, b, msg)
+    local message = msg or format('%s expected to be different from %s', a, b)
+    assert(a ~= b, message)
   end,
 
-  ok = function(a)
-    assert(a)
+  ok = function(a, msg)
+    local message = msg or format('%s is not true', a)
+    assert(a, message)
   end,
 
-  not_ok = function(a)
-    assert(not a)
+  not_ok = function(a, msg)
+    local message = msg or format('%s is not false', a)
+    assert(not a, message)
   end,
 
-  throw = function(message, method, params)
+  throw = function(method, params, raised_message, msg)
     local raised_error = false
 
     xpcall(function()
       method(table.unpack(params))
     end, function(err)
       raised_error = true
-      assert(string.find(err, message))
+      assert(string.find(err, raised_message))
     end)
 
-    if not raised_error then error('should throw error') end
+    if not raised_error then error(msg or 'should throw error') end
   end,
 
-  delta = function(a, b, delta)
+  delta = function(a, b, delta, msg)
     local diff = math.abs(a - b)
     delta = delta or 0.00001
+    local message = msg or format('%s%s%s expected but was %s%s%s',
+          colors.green[1], a, colors.green[2], colors.red[1], b, colors.red[2])
 
     if a == b then
       return true
     elseif a == 0 or b == 0 then
-      assert(diff < delta)
+      assert(diff < delta, message)
     else
       local abs_a = math.abs(a)
       local abs_b = math.abs(b)
 
-      assert((diff / (abs_a + abs_b)) < delta)
+      assert((diff / (abs_a + abs_b)) < delta, message)
     end
   end
 }
 
-local format = string.format
-
 local function test(name, callback)
   xpcall(function()
     callback(assertions)
-    print(format('%s[pass]%s %s', '\27[32m', '\27[0m', name))
+    print(format('%s[pass]%s %s', colors.green[1], colors.green[2], name))
   end, function(err)
-    print(format('%s[fail]%s %s : %s','\27[31m', '\27[0m', name, err))
+    print(format('%s[fail]%s %s : %s', colors.red[1], colors.red[2], name, err))
     os.exit(1)
   end)
 end
