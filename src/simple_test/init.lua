07f669fcca81882c355cf190d71bc9d64c480fcf
Cole Granof <bandaloo@icloud.com>
2023-01-31 11:40:04 -0500

    added passed unsuccessfully

--- src/simple_test/init.lua
+++ src/simple_test/init.lua
@@ -5,7 +5,12 @@ local format = string.format
 local function test(name, callback, expect_to_fail)
   xpcall(function()
     callback(assertions)
-    print(format('%s[pass]%s %s', colors.green[1], colors.green[2], name))
+    if not expect_to_fail then
+      print(format('%s[pass]%s %s', colors.green[1], colors.green[2], name))
+    else
+      print(format('%s[passed unsuccessfully...]%s %s',
+            colors.red[1], colors.red[2], name))
+    end
   end, function(err)
     if not expect_to_fail then
       print(format('%s[fail]%s %s : %s', colors.red[1], colors.red[2], name, err))
